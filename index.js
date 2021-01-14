72faad49a1a67bc03a733e9cbe6ce10839af00d1
anan_p <se56160422@gmail.com>
2021-01-14 13:53:49 +0700

    new

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/', (req, res) => {
   
-    res.send("results :"+req.body.TransactionId)
+    res.send("results :"+req.params("TransactionId"))
   console.log("call back")
 
 })
